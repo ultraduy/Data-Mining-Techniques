2026878fa2a3a0d4c7b57bb34fd6181d770ab5f1
Duy <duy_nl@hotmail.com>
2022-04-14 13:57:16 +0200

    Y meeveranderd

--- python file.py
+++ python file.py
@@ -17,18 +17,22 @@ Assignment 1 DMT
 data = pd.read_csv("ODI-2022.csv", delimiter= ";")
 print(data.describe())
 
-vY = data["What is your gender?"]
-mX = data[["What is your stress level (0-100)?", "Give a random number"]]
+pre_data = data[["What is your gender?","What is your stress level (0-100)?", "Give a random number"]]
 
-#mX = mX[pd.to_numeric(mX["Give a random number"], errors='coerce').notnull()]
-#mX = mX[pd.to_numeric(mX["What is your stress level (0-100)?"], errors='coerce').notnull()]
+#select integers (preprocessing)
+pre_data = pre_data[check_integer(pre_data["Give a random number"])]
+pre_data = pre_data[check_integer(pre_data["What is your stress level (0-100)?"])]
 
-#select integers
-mX = mX[check_integer(mX["Give a random number"])]
-mX = mX[check_integer(mX["What is your stress level (0-100)?"])]
+pre_data["What is your stress level (0-100)?"] = pre_data["What is your stress level (0-100)?"].astype('float')
+pre_data["Give a random number"] = pre_data["Give a random number"].astype('float')
 
-mX = mX.astype('float')
-mX = mX[mX["What is your stress level (0-100)?"]<=100]
-mX = mX[mX["What is your stress level (0-100)?"]>=0]
+pre_data = pre_data[pre_data["What is your stress level (0-100)?"]<=100]
+pre_data = pre_data[pre_data["What is your stress level (0-100)?"]>=0]
 
+
+#obtain final data
+vY = pre_data["What is your gender?"]
+
+mX = pre_data[["What is your stress level (0-100)?", "Give a random number"]]
 mX = mX.astype('int')
+
